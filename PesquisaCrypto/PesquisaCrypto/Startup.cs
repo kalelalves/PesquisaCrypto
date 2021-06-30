e95645c88e30f06d2652bc3a28050f4c2d821aac
Kalel Jamil Alves <kalel_fipp@hotmail.com>
2021-06-30 00:06:40 -0300

    CONEXÃO COM O BANCO OK

--- PesquisaCrypto/PesquisaCrypto/Startup.cs
+++ PesquisaCrypto/PesquisaCrypto/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PesquisaCrypto.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +32,9 @@ namespace PesquisaCrypto
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+
+            services.AddDbContext<MoedasContexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("MoedasConexao")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
